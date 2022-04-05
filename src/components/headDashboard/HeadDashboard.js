b4a4d2e28f261b2c4507816825a8cfe50a2845c6
Future64 <developement.future@gmail.com>
2022-04-05 13:02:19 +0200

    state change

--- src/components/headDashboard/HeadDashboard.js
+++ src/components/headDashboard/HeadDashboard.js
@@ -1,13 +1,38 @@
 import { useState, useEffect, useContext, useRef } from 'react'
-import { Routes, Route } from 'react-router-dom'
+import axios from 'axios'
 import { useDispatch, useSelector } from 'react-redux'
 import { setFirstName, setLastName, setEmail, setToken } from '../../redux/reducer'
 import './HeadDashboard.css'
 
 const HeadDashboard = () => {
+  const LOGIN_PROFILE = 'http://localhost:3001/api/v1/user/profile'
   const dispatch = useDispatch()
   const user = useSelector((state) => {return state})
   const [toggle, setToggle] = useState(false)
+  const [userNames, setUserNames] = useState({
+    firstname: '',
+    lastname: ''
+  })
+
+  async function putRequest() {
+    return axios
+    .put(
+      LOGIN_PROFILE,
+        { userNames },
+        {
+            headers: {
+                Authorization: `Bearer ` + user.user.token,
+            }
+        }
+    )
+    .then(function (response) {
+        return response;
+    })
+    .catch(function (error) {
+        return error.response;
+    });
+}
+  
 
   const changeToggle = () => {
     toggle === false ? setToggle(true) : setToggle(false)
@@ -37,16 +62,18 @@ const HeadDashboard = () => {
   const editZone = (
     <>
       <h1>Welcome back</h1>
+      <form>
       <input className="firstName input" type="text" name="firstName" placeholder="tony" />
       <input className="familyName input" type="text" name="familyName" placeholder="Jarvis" />
       <div className="buttonBox">
-        <button className="save-button btn" onClick={changeName}>
+        <button className="save-button btn"  onClick={changeName}>
           Save
         </button>
         <button className="cancel-button btn" onClick={changeToggle}>
           Cancel
         </button>
       </div>
+      </form>
     </>
   )
 
