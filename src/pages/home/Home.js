c72e7896ad6f5c599baa0911a67ce52a28f485a2
Future64 <developement.future@gmail.com>
2022-04-15 13:42:55 +0200

    delete useless dispatch in home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import {useSelector } from 'react-redux'
 import './Home.css'
 import Header from '../../components/header/Header'
 import Sectionhome from '../../components/sectionHome/SectionHome'
